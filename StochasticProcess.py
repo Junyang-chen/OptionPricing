90a4bdaf1d92534cbd6e9cb9a7aee8311923377a
Tony Chen <junyang.chen@outlook.com>
2017-02-23 00:10:48 -0500

    OptionPricer add GeometricBrownianMotionGenerator and bug fixed

--- StochasticProcess.py
+++ StochasticProcess.py
@@ -1,6 +1,7 @@
 
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 from abc import ABCMeta, abstractclassmethod
 
 """
@@ -30,19 +31,71 @@ class StochasticProcessGenerator(metaclass=ABCMeta):
 
 class BrownianMotionGenerator(StochasticProcessGenerator):
 
-    def __init__(self, numofPoints=0):
-        self._numofPoints = numofPoints
+    def __init__(self, timetoexp=1, mu=0, vol=1):
+        self._numofPoints = int(timetoexp*252)
+        self.timetoexp = timetoexp
+        self.mu = mu
+        self.vol = vol
+        self.dt = timetoexp/self._numofPoints
 
     def generatePath(self):
-        initialseries = np.random.normal(0, 1, self._numofPoints)
+        initialseries = self.vol*np.random.normal(0, 1, self._numofPoints)*np.sqrt(self.dt) + self.dt * self.mu
         initialseries = np.cumsum(initialseries)
         result = pd.DataFrame(initialseries)
-        # result.index = np.arange(1, len(result) + 1)
+        result.index = result.index/252
         return result
 
     def generatePaths(self, numofPaths):
-        initialseries = np.random.randn(numofPaths, self._numofPoints)
-        initialseries = np.cumsum(initialseries)
-        result = pd.DataFrame(np.random.randn(numofPaths, self._numofPoints))
-        # result.index = np.arange(1, len(result) + 1)
+        initialseries = self.vol*np.random.randn(numofPaths, self._numofPoints)*np.sqrt(self.dt) + self.dt * self.mu
+        initialseries = np.transpose(initialseries)
+        initialseries = np.cumsum(initialseries, axis=0)
+        result = pd.DataFrame(initialseries)
+        result.index = result.index / 252
         return result
+
+class GeometricBrownianMotionGenerator(StochasticProcessGenerator):
+
+    def __init__(self, timetoexp=1, mu=0, vol=1):
+        self._numofPoints = int(timetoexp*252)
+        self.timetoexp = timetoexp
+        self.mu = mu
+        self.vol = vol
+        self.dt = timetoexp/self._numofPoints
+
+    def generatePath(self):
+        initialseries = self.vol*np.random.normal(0, 1, self._numofPoints)*np.sqrt(self.dt)\
+                        + self.dt * (self.mu - self.vol**2/2)
+        initialseries = np.cumsum(initialseries)
+        expfunc = lambda t: np.exp(t)
+        vfunc = np.vectorize(expfunc)
+        initialseries = vfunc(initialseries)
+        result = pd.DataFrame(initialseries)
+        result.index = result.index/252
+        return result
+
+    def generatePaths(self, numofPaths):
+        initialseries = self.vol*np.random.randn(numofPaths, self._numofPoints)*np.sqrt(self.dt)\
+                        + self.dt * (self.mu - self.vol**2/2)
+        initialseries = np.transpose(initialseries)
+        initialseries = np.cumsum(initialseries, axis=0)
+        expfunc = lambda t: np.exp(t)
+        vfunc = np.vectorize(expfunc)
+        initialseries = vfunc(initialseries)
+        result = pd.DataFrame(initialseries)
+        result.index = result.index / 252
+        return result
+
+if __name__ == '__main__':
+    brownianmotion = BrownianMotionGenerator(1, 0.3, 0.1)
+    onepath = brownianmotion.generatePath()
+    fig, ax = plt.subplots(2,2)
+    onepath.plot(ax=ax[0,0], title='brownianmotion')
+    multiplepaths = brownianmotion.generatePaths(100)
+    multiplepaths.plot(ax=ax[0,1], title='brownianmotion multiple')
+
+    geometricbrownianmotion = GeometricBrownianMotionGenerator(1, 0.3, 0.1)
+    onepath = geometricbrownianmotion.generatePath()
+    onepath.plot(ax=ax[1, 0], title='geometricbrownianmotion')
+    multiplepaths = geometricbrownianmotion.generatePaths(100)
+    multiplepaths.plot(ax=ax[1,1], title='brownianmotion multiple')
+    plt.show()
